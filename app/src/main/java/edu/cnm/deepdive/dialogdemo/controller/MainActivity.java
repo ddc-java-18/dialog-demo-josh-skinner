745ffa3cd48448354843fb90ecc8652da1bdc8e4
jskinner <jskinner18@cnm.edu>
2024-07-15 16:50:29 -0600

    Start of permissions flow.

--- app/src/main/java/edu/cnm/deepdive/dialogdemo/controller/MainActivity.java
+++ app/src/main/java/edu/cnm/deepdive/dialogdemo/controller/MainActivity.java
@@ -1,7 +1,13 @@
 package edu.cnm.deepdive.dialogdemo.controller;
 
+import android.Manifest.permission;
+import android.content.pm.PackageManager;
+import android.util.Log;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.navigation.NavController;
 import androidx.navigation.fragment.NavHostFragment;
 import androidx.navigation.ui.AppBarConfiguration;
@@ -10,6 +16,8 @@ import edu.cnm.deepdive.dialogdemo.R;
 import edu.cnm.deepdive.dialogdemo.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+
+  private static final int PERMISSIONS_REQUEST_CODE = 17763535;
 
   private ActivityMainBinding binding;
   private NavController navController;
@@ -20,6 +28,11 @@ public class MainActivity extends AppCompatActivity {
     binding = ActivityMainBinding.inflate(getLayoutInflater());
     setContentView(binding.getRoot());
     setupNavigation();
+    boolean shouldExplain = shouldExplainCameraPermission();
+    boolean shouldRequest = shouldRequestCameraPermission();
+    Log.d(getClass().getSimpleName(), String.format("explain: %1$b; request: %2$s", shouldRequest, shouldExplain));
+    // TODO: 7/15/24 : Explains permissions as necessary.
+    requestPermissions(new String[]{android.Manifest.permission.CAMERA}, PERMISSIONS_REQUEST_CODE);
   }
 
   @Override
@@ -27,15 +40,38 @@ public class MainActivity extends AppCompatActivity {
     return navController.navigateUp() || super.onSupportNavigateUp();
   }
 
-  private void setupNavigation(){
-    AppBarConfiguration config = new AppBarConfiguration.Builder(R.id.main_fragment)
-        .build();
-    //noinspection DataFlowIssue
-    navController = ((NavHostFragment) getSupportFragmentManager()
-        .findFragmentById(R.id.nav_host_fragment))
-        .getNavController();
-    NavigationUI.setupActionBarWithNavController(this, navController, config);
-
+  @Override
+  public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
+      @NonNull int[] grantResults) {
+    if (requestCode == PERMISSIONS_REQUEST_CODE) {
+      if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+        // TODO: 7/15/24 : Remember that we have permission to use the camera.
+      }
+    } else {
+      super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    }
   }
+
+
+private void setupNavigation() {
+  AppBarConfiguration config = new AppBarConfiguration.Builder(R.id.main_fragment)
+      .build();
+  //noinspection DataFlowIssue
+  navController = ((NavHostFragment) getSupportFragmentManager()
+      .findFragmentById(R.id.nav_host_fragment))
+      .getNavController();
+  NavigationUI.setupActionBarWithNavController(this, navController, config);
+
+}
+
+private boolean shouldRequestCameraPermission() {
+  return ContextCompat.checkSelfPermission(this, permission.CAMERA)
+      != PackageManager.PERMISSION_GRANTED;
+}
+
+private boolean shouldExplainCameraPermission() {
+  return ActivityCompat.shouldShowRequestPermissionRationale(
+      this, android.Manifest.permission.CAMERA);
+}
 
 }
--- app/src/main/java/edu/cnm/deepdive/dialogdemo/controller/MainFragment.java
+++ app/src/main/java/edu/cnm/deepdive/dialogdemo/controller/MainFragment.java
@@ -1,17 +1,21 @@
 package edu.cnm.deepdive.dialogdemo.controller;
 
+import android.Manifest.permission;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.Navigation;
 import edu.cnm.deepdive.dialogdemo.databinding.FragmentMainBinding;
 
 public class MainFragment extends Fragment {
-  
+
   private FragmentMainBinding binding;
 
   @Nullable
@@ -21,18 +25,18 @@ public class MainFragment extends Fragment {
     binding = FragmentMainBinding.inflate(inflater, container, false);
     binding.showEditFragment.setOnClickListener((v) ->
         Navigation.findNavController(binding.getRoot())
-    .navigate(MainFragmentDirections.navigateToEditFragment()));
+            .navigate(MainFragmentDirections.navigateToEditFragment()));
     // TODO: 7/15/24 : Attach listeners to widgets in binding fields. 
     return binding.getRoot();
-
-    
   }
 
   @Override
   public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
     super.onViewCreated(view, savedInstanceState);
-    // TODO: 7/15/24 : Attach to viewmodel(s) and observe livedata of interest.
+    // TODO: 7/15/24 : Show simple dialog to explain.
   }
+  // TODO: 7/15/24 : Attach to viewmodel(s) and observe livedata of interest.
+
 
   @Override
   public void onDestroyView() {
@@ -40,3 +44,6 @@ public class MainFragment extends Fragment {
     super.onDestroyView();
   }
 }
+
+  
+
